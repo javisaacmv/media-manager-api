f31a0c7ad61fab9a6a3b738508b8d5e3267d762d
Javier Montano <javier.montano@imaginelearning.com>
2024-04-22 18:10:38 -0700

    aws

--- functions/api.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv'
-import serverless from "serverless-http";
-import Server from '../models/server';
-dotenv.config
-
-const server = new Server()
-
-export const handler = serverless(server.serve);
